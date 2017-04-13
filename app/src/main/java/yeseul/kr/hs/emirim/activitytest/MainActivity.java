55ae1c49b7e054d86b8f9eddaf172956f63a38fd
leeysi <yeseul726@e-mirim.hs.kr>
2017-04-13 20:52:05 +0900

    #5 Input Event Programming

--- app/src/main/java/yeseul/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/activitytest/MainActivity.java
@@ -5,20 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editName, editTel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button butMain = (Button)findViewById(R.id.but_main);
         butMain.setOnClickListener(butMainHandler);
+        editName = (EditText)findViewById(R.id.edit_name);
+        editTel = (EditText)findViewById(R.id.edit_tel);
     }
     View.OnClickListener butMainHandler = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent Intent = new Intent(MainActivity.this, SecondActivity.class);
+            String name = editName.getText().toString();
+            String tel = editTel.getText().toString();
+            Intent.putExtra("name", name); //두번째가 실제 값을 저장할 변수(값) 첫번째는 이름
+            Intent.putExtra("tel", tel);
             startActivity(Intent);
         }
     }; //익명클래스 객체이기 때문에 끝에 ;을 붙임
--- app/src/main/java/yeseul/kr/hs/emirim/activitytest/SecondActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/activitytest/SecondActivity.java
@@ -1,19 +1,25 @@
 package yeseul.kr.hs.emirim.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    String name, tel; //익명클래스에서 사용하기 위해서는 전역변수로 선언해줘야함
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Button butBack = (Button)findViewById(R.id.but_second);
         butBack.setOnClickListener(butBackHandler);
-
+        Intent Intent = getIntent();
+        name = Intent.getStringExtra("name"); //이름이 "name"이라고 붙어있는 값을 가져온다는 의미
+        tel = Intent.getStringExtra("tel");
+        TextView textResult = (TextView)findViewById(R.id.text_result);
+        textResult.setText("* 성명 : " + name + "\n * 전화번호 : " + tel);
     }
     View.OnClickListener butBackHandler = new View.OnClickListener() {
         @Override
